a733628e342c0230d9d47b86c7e859d6fa7c83bd
Vincent Bernat <vincent@bernat.ch>
2022-08-13 14:56:37 +0200

    Add basic typing

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -33,39 +33,39 @@ import logging
 import logging.handlers
 import os
 import sys
+from typing import List, Union
 
 logger = logging.getLogger(os.path.splitext(os.path.basename(sys.argv[0]))[0])
 
 
-class CustomFormatter(argparse.RawDescriptionHelpFormatter,
-                      argparse.ArgumentDefaultsHelpFormatter):
+class CustomFormatter(
+    argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter
+):
     pass
 
 
-def parse_args(args=sys.argv[1:]):
+def parse_args(args: List[str] = sys.argv[1:]) -> argparse.Namespace:
     """Parse arguments."""
     parser = argparse.ArgumentParser(
-        description=sys.modules[__name__].__doc__,
-        formatter_class=CustomFormatter)
+        description=sys.modules[__name__].__doc__, formatter_class=CustomFormatter
+    )
 
-    g = parser.add_mutually_exclusive_group()
-    g.add_argument("--debug", "-d", action="store_true",
-                   default=False,
-                   help="enable debugging")
-    g.add_argument("--silent", "-s", action="store_true",
-                   default=False,
-                   help="don't log")
+    g1 = parser.add_mutually_exclusive_group()
+    g1.add_argument(
+        "--debug", "-d", action="store_true", default=False, help="enable debugging"
+    )
+    g1.add_argument(
+        "--silent", "-s", action="store_true", default=False, help="don't log"
+    )
 
     # TODO: modify these options
-    g = parser.add_argument_group("fizzbuzz settings")
-    g.add_argument("--fizz", metavar="N",
-                   default=3,
-                   type=int,
-                   help="Modulo value for fizz")
-    g.add_argument("--buzz", metavar="N",
-                   default=5,
-                   type=int,
-                   help="Modulo value for buzz")
+    g2 = parser.add_argument_group("fizzbuzz settings")
+    g2.add_argument(
+        "--fizz", metavar="N", default=3, type=int, help="Modulo value for fizz"
+    )
+    g2.add_argument(
+        "--buzz", metavar="N", default=5, type=int, help="Modulo value for buzz"
+    )
 
     parser.add_argument("start", type=int, help="Start value")
     parser.add_argument("end", type=int, help="End value")
@@ -73,7 +73,7 @@ def parse_args(args=sys.argv[1:]):
     return parser.parse_args(args)
 
 
-def setup_logging(options):
+def setup_logging(options: argparse.Namespace) -> None:
     """Configure logging."""
     root = logging.getLogger("")
     root.setLevel(logging.WARNING)
@@ -81,21 +81,20 @@ def setup_logging(options):
     if not options.silent:
         if not sys.stderr.isatty():
             facility = logging.handlers.SysLogHandler.LOG_DAEMON
-            sh = logging.handlers.SysLogHandler(address='/dev/log',
-                                                facility=facility)
-            sh.setFormatter(logging.Formatter(
-                "{0}[{1}]: %(message)s".format(
-                    logger.name,
-                    os.getpid())))
+            sh = logging.handlers.SysLogHandler(address="/dev/log", facility=facility)
+            sh.setFormatter(
+                logging.Formatter(
+                    "{0}[{1}]: %(message)s".format(logger.name, os.getpid())
+                )
+            )
             root.addHandler(sh)
         else:
             ch = logging.StreamHandler()
-            ch.setFormatter(logging.Formatter(
-                "%(levelname)s[%(name)s] %(message)s"))
+            ch.setFormatter(logging.Formatter("%(levelname)s[%(name)s] %(message)s"))
             root.addHandler(ch)
 
 
-def fizzbuzz(n, fizz, buzz):
+def fizzbuzz(n: int, fizz: int, buzz: int) -> Union[str, int]:
     """Compute fizzbuzz nth item given modulo values for fizz and buzz.
 
     >>> fizzbuzz(5, fizz=3, buzz=5)
@@ -119,13 +118,13 @@ def fizzbuzz(n, fizz, buzz):
     return n
 
 
-def main(options):
+def main(options: argparse.Namespace) -> List[str]:
     """Compute a fizzbuzz set of strings and return them as an array."""
-    logger.debug("compute fizzbuzz from {} to {}".format(options.start,
-                                                         options.end))
-    return [str(fizzbuzz(i,
-                         fizz=options.fizz, buzz=options.buzz))
-            for i in range(options.start, options.end+1)]
+    logger.debug("compute fizzbuzz from {} to {}".format(options.start, options.end))
+    return [
+        str(fizzbuzz(i, fizz=options.fizz, buzz=options.buzz))
+        for i in range(options.start, options.end + 1)
+    ]
 
 
 if __name__ == "__main__":
@@ -141,18 +140,21 @@ if __name__ == "__main__":
 
 
 # Unit tests
-import pytest                   # noqa: E402
-import shlex                    # noqa: E402
+import pytest  # noqa: E402
+import shlex  # noqa: E402
 
 
-@pytest.mark.parametrize("args, expected", [
-    ("0 0", ["fizzbuzz"]),
-    ("3 5", ["fizz", "4", "buzz"]),
-    ("9 12", ["fizz", "buzz", "11", "fizz"]),
-    ("14 17", ["14", "fizzbuzz", "16", "17"]),
-    ("14 17 --fizz=2", ["fizz", "buzz", "fizz", "17"]),
-    ("17 20 --buzz=10", ["17", "fizz", "19", "buzz"]),
-])
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        ("0 0", ["fizzbuzz"]),
+        ("3 5", ["fizz", "4", "buzz"]),
+        ("9 12", ["fizz", "buzz", "11", "fizz"]),
+        ("14 17", ["14", "fizzbuzz", "16", "17"]),
+        ("14 17 --fizz=2", ["fizz", "buzz", "fizz", "17"]),
+        ("17 20 --buzz=10", ["17", "fizz", "19", "buzz"]),
+    ],
+)
 def test_main(args, expected):
     options = parse_args(shlex.split(args))
     assert main(options) == expected
